249ab43b5f0edf465a8f9b9f92d82d9a1c97544a
Mike <mikehammond32@gmail.com>
2021-03-01 05:44:45 -0500

    added user input

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,5 +1,7 @@
 package com.pluralsight.calcengine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -19,7 +21,25 @@ public class Main {
 		else
 			System.out.println("Please provide an operation code and 2 numeric values");
     }
-    private static void handleCommandLine(String[] args) {
+
+    static void executeInteractively() {
+    	System.out.println("Enter an operation and two numbers:");
+		Scanner scanner = new Scanner(System.in);
+		String userInput = scanner.nextLine();
+		String [] parts = userInput.split(regex:" ");
+		performOperation(parts);
+
+	}
+
+	private static void performOperation(String[] parts) {
+    	char opCode = opCodeFromString(parts[0]);
+    	double leftVal = valueFromWord(parts[1]);
+    	double rightVal = valueFromWord(parts[2]);
+    	double result = execute(opCode, leftVal, rightVal);
+    	System.out.println(result);
+	}
+
+	private static void handleCommandLine(String[] args) {
     	char opCode = args[0].charAt(0);
     	double leftVal = Double.parseDouble(args[1]);
     	double rightVal = Double.parseDouble(args[2]);
@@ -49,4 +69,24 @@ public class Main {
 				break;
 		}
 		return result;
-}}
+	}
+
+	static char opCodeFromString(String operationName) {
+    	char opCode = operationName.charAt(0);
+    	return opCode;
+	}
+
+	static double valueFromWord(String word) {
+		String[] numberWords = {
+				"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+		};
+		double value = 0d;
+		for (int index = 0; index < numberWords.length; index++) {
+			if(word.equals(numberWords[index])) {
+				value = index;
+				break;
+			}
+		}
+		return value;
+	}
+}
